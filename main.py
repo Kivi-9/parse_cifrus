810eb5f464ee08938bab320888233c38b8c7b514
Kivi-9 <utidinery@ya.ru>
2024-11-28 09:23:08 +0300

    Fix

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def parse(url):
         'start': '0',
         'limit': '120'
     }
+
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36"
     }
